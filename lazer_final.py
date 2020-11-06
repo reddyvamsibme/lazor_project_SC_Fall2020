b93ffea4e4e2c512434f5110de612095e521f2f8
Reddy-Vamsi <reddy.vamsi@hotmail.com>
2020-11-05 21:31:39 -0500

    Creating a class for lazor

--- lazer_final.py
+++ lazer_final.py
@@ -8,3 +8,18 @@ Emad Mohammed Naveed <enaveed1@jhu.edu>
 # Import packages
 from itertools import combinations
 from PIL import Image, ImageDraw
+
+
+
+
+class Lazor:
+    '''
+        This class estimates all possible combinations to find the solution
+        Step 1: Sorting A blocks in possible 'o' positions to get all
+                combinations
+        Step 2: With leftover o positions, do similar combination search
+                for B, C
+        Step 3: Create possible combinations of A, B, C with available
+                'o' positions and locked blocks within the grid
+    '''
+    
