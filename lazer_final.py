a71d803bf92b31c5e879614a90559127aa21b832
Emad Naveed <emad@Emads-MacBook-Air-13.local>
2020-11-05 21:13:05 -0500

    Created read function

--- lazer_final.py
+++ lazer_final.py
@@ -8,3 +8,16 @@ Emad Mohammed Naveed <enaveed1@jhu.edu>
 # Import packages
 from itertools import combinations
 from PIL import Image, ImageDraw
+
+def read():
+	if not filename.lower().endswith('.bff'):
+            raise SystemExit("Invalid File type, try with .bff files")
+        # Initializing position variables and block list
+        A, B, C = 0, 0, 0
+        o_l, x_l, A_l, B_l, C_l, lazers, points = (
+            [] for i in range(7))
+
+        # Open the file with .bff extensions
+        file = open(filename, 'r')
+        # Read the information line by line
+        lines = file.read().splitlines()
