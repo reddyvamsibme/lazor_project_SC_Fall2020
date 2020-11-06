e5512383553a94df1d17cfb17c9938077234bad0
Reddy-Vamsi <reddy.vamsi@hotmail.com>
2020-11-05 21:32:57 -0500

    Defined __initial__ method for variables

--- lazer_final.py
+++ lazer_final.py
@@ -22,4 +22,14 @@ class Lazor:
         Step 3: Create possible combinations of A, B, C with available
                 'o' positions and locked blocks within the grid
     '''
-    
+
+    def __init__(self, dataset1, dataset2):
+        
+        self.o_l = dataset1['o_l']
+        self.size = dataset1['Size']
+        self.lazers = dataset1['Lazers']
+        self.points = dataset1['Points']
+        self.A = dataset1['A']
+        self.B = dataset1['B']
+        self.C = dataset1['C']
+        self.dataset2 = dataset2
