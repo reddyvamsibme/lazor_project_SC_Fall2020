0ea555cca6cc0ba33769b123b6ecd9535bd42bb6
Vamsi Reddy <60086641+reddyvamsibme@users.noreply.github.com>
2020-11-05 21:09:26 -0500

    Update lazer_final.py

--- lazer_final.py
+++ lazer_final.py
@@ -1,1 +1,10 @@
-
+'''
+Software Carpentry, Fall 2020
+Lazors project
+Submitted by
+Vamsi Reddy <vreddy17@jhu.edu>
+Emad Mohammed Naveed <enaveed1@jhu.edu>
+'''
+# Import packages
+from itertools import combinations
+from PIL import Image, ImageDraw
